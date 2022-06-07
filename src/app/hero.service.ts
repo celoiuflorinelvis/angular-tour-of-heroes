2178d280695434ea907a9459c29fc75672c07b2d
celoiuflorinelvis <celoiuflorinelvis@yahoo.co.uk>
2022-06-07 17:00:17 +0300

    add CRUD method to service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,13 +2,16 @@ import { Injectable } from '@angular/core';
 import { Observable, of, tap, catchError } from 'rxjs';
 import { Hero } from './hero';
 import { MessagesService } from './messages.service';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
   private heroesUrl = 'api/heroes';
+  private options = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
 
   constructor(
     private http: HttpClient,
@@ -34,6 +37,27 @@ export class HeroService {
         this.messagesService.addMessage(`Hero ${_.name} is selected.`)
       ),
       catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.options).pipe(
+      tap((_) => this.log(`Hero ${hero.id} name was changed to: ${hero.name}`)),
+      catchError(this.handleError<Hero>(`updateHero id=${hero.id}`))
+    );
+  }
+
+  deleteHero(id: number): Observable<any> {
+    return this.http.delete(`${this.heroesUrl}/${id}`, this.options).pipe(
+      tap((_) => this.log(`Hero ${id} was deleted.`)),
+      catchError(this.handleError<any>('deleteHero: ' + id))
+    );
+  }
+
+  addHero(hero: Hero): Observable<Hero | undefined> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.options).pipe(
+      tap((_) => this.log('New hero was saved.')),
+      catchError(this.handleError<Hero>('addHero'))
     );
   }
 
